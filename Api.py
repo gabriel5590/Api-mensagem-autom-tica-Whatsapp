bc042f8a4912626ac3bc8f364638fb0584ada021
gabriel5590 <166563651+gabriel5590@users.noreply.github.com>
2024-05-09 23:22:54 -0300

    Create Api.py
    Alterado codigo donte

---
+++ Api.py
@@ -0,0 +1,16 @@
+ #By Gabriel #
+
+
+
+import pywhatkit 
+telefone_numero = 'NUMERO DE TELEFONE'
+#Mensagem para mandar"
+mensagem = 'Mensagem enviada'
+horas = 13
+minutos = 42
+pywhatkit.sendwhatmsg(telefone_numero, mensagem, horas, minutos)
+print("Mensagem enviada")
+
+
+#Altere o código colocando em telefone_numero o numero de telefone desejado.
+#altere a mensagem para a mensagem desejada.
